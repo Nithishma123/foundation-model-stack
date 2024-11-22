e334d02b7e4bdb4f7b8a4b5a6ef7e77fcc50f29c
Nithishma123 <62936146+Nithishma123@users.noreply.github.com>
2024-11-21 22:42:48 -0500

    Update modeling_gpt_bigcode_hf.py

--- fms/models/hf/gpt_bigcode/modeling_gpt_bigcode_hf.py
+++ fms/models/hf/gpt_bigcode/modeling_gpt_bigcode_hf.py
@@ -2,20 +2,17 @@ from typing import Optional, Tuple
 
 import torch
 import torch.nn as nn
+from torch.distributed._tensor import distribute_tensor, DeviceMesh, Shard
 from transformers import PretrainedConfig
 from transformers.modeling_outputs import BaseModelOutputWithPastAndCrossAttentions
 
 from fms.models.gpt_bigcode import GPTBigCode, GPTBigCodeHeadless
-from fms.models.hf.gpt_bigcode.configuration_gpt_bigcode_hf import (
-    HFAdaptedGPTBigCodeConfig,
-)
+from fms.models.hf.gpt_bigcode.configuration_gpt_bigcode_hf import HFAdaptedGPTBigCodeConfig
 from fms.models.hf.lm_head_mixins import LMHeadModelLMHeadMixin
 from fms.models.hf.modeling_hf_adapter import HFDecoder, HFDecoderModelArchitecture
 
 
 class HFAdaptedGPTBigCodeDecoder(HFDecoder):
-    """Adapter for the GPTBigCodeDecoder"""
-
     def __init__(self, model: nn.Module, config: PretrainedConfig):
         super().__init__(model, config, attention_mask_dim=3)
 
@@ -26,12 +23,16 @@ class HFAdaptedGPTBigCodeDecoder(HFDecoder):
         position_ids: Optional[torch.LongTensor] = None,
         past_key_values: Optional[Tuple[torch.Tensor]] = None,
         use_cache: Optional[bool] = None,
-        attn_algorithm: Optional[
-            str
-        ] = None,  # this can be passed in from top most forward
+        attn_algorithm: Optional[str] = None,
+        enable_sequence_parallelism: bool = False,
+        shard_dim: int = 1,
         *args,
         **kwargs,
     ) -> BaseModelOutputWithPastAndCrossAttentions:
+        if enable_sequence_parallelism and input_ids is not None:
+            shard_size = input_ids.size(shard_dim) // torch.distributed.get_world_size()
+            input_ids = torch.split(input_ids, shard_size, dim=shard_dim)[torch.distributed.get_rank()]
+
         output, cache = self.model(
             x=input_ids,
             mask=attention_mask,
@@ -47,54 +48,26 @@ class HFAdaptedGPTBigCodeDecoder(HFDecoder):
 
 
 class HFAdaptedGPTBigCodeHeadless(HFDecoderModelArchitecture):
-    """This is the Adapter for the base granite architecture"""
-
-    # attributes required by HF
     config_class = HFAdaptedGPTBigCodeConfig
     base_model_prefix = "hf_adapted_gpt_bigcode"
 
-    def __init__(
-        self,
-        config: PretrainedConfig,
-        decoder: Optional[GPTBigCodeHeadless] = None,
-        embedding: Optional[nn.Module] = None,
-        *args,
-        **kwargs,
-    ):
-        # in the case we have not yet received the encoder/decoder/embedding, initialize it here
-        if decoder is None or embedding is None:
-            params = config.to_dict()
-            params["pad_id"] = params.pop("pad_token_id")
-            model = GPTBigCode(**params)
-            decoder = model.base_model if decoder is None else decoder
-            embedding = model.base_model.embedding if embedding is None else embedding
+    def __init__(self, config: PretrainedConfig, *args, **kwargs):
+        device_mesh = DeviceMesh("cuda", torch.arange(torch.cuda.device_count()))
+        placement = [Shard(0)]
 
-        # these are now huggingface compatible
-        decoder = HFAdaptedGPTBigCodeDecoder(decoder, config)
-        super().__init__(decoder, embedding, config, *args, **kwargs)
+        if "embedding" in kwargs:
+            kwargs["embedding"] = distribute_tensor(kwargs["embedding"], device_mesh, placement)
+        super().__init__(*args, config=config, **kwargs)
 
 
-class HFAdaptedGPTBigCodeForCausalLM(
-    LMHeadModelLMHeadMixin, HFAdaptedGPTBigCodeHeadless
-):
+class HFAdaptedGPTBigCodeForCausalLM(LMHeadModelLMHeadMixin, HFAdaptedGPTBigCodeHeadless):
     _keys_to_ignore_on_load_missing = [r"lm_head.weight"]
-    _tied_weights_keys = ["embedding.weight", "lm_head.weight"]
 
     def __init__(self, config: HFAdaptedGPTBigCodeConfig, *args, **kwargs):
-        super().__init__(config=config, bias=False, *args, **kwargs)
+        super().__init__(config=config, *args, **kwargs)
 
     def _tie_weights(self):
-        # We know that FMS always saves the LM head weight, so ensure the right pointer is shared
-        self.embedding.weight = self.lm_head.weight
+        device_mesh = DeviceMesh("cuda", torch.arange(torch.cuda.device_count()))
+        placement = [Shard(0)]
+        self.embedding.weight = distribute_tensor(self.lm_head.weight, device_mesh, placement)
         self.decoder.model.embedding.weight = self.embedding.weight
-
-    @classmethod
-    def _hf_model_from_fms(
-        cls, model: nn.Module, config: HFAdaptedGPTBigCodeConfig
-    ) -> "HFAdaptedGPTBigCodeForCausalLM":
-        return cls(
-            config=config,
-            decoder=model.base_model,
-            embedding=model.base_model.embedding,
-            lm_head=model.head,
-        )
