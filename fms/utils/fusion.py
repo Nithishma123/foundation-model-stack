906fee48f11780d8d907a7b5dbbc9dea2c42f61f
Nithishma123 <62936146+Nithishma123@users.noreply.github.com>
2024-12-04 16:11:28 -0500

    Update fusion.py

--- fms/utils/fusion.py
+++ fms/utils/fusion.py
@@ -4,6 +4,8 @@ import torch.nn as nn
 
 def _maybe_unfuse_weights(module: nn.Module):
     if hasattr(module, "unfuse_weights"):
+        if isinstance(module.weight, torch.Tensor) and module.weight.is_distributed():
+            module.weight = module.weight.redistribute()
         result = module.unfuse_weights()
         del module
     else:
@@ -27,9 +29,15 @@ def apply_unfuse_weights(module: nn.Module) -> nn.Module:
     with torch.no_grad():
         wrapped = _maybe_unfuse_weights(module)
         if wrapped is not module:
+            if wrapped.weight.is_distributed():
+                    wrapped.weight = wrapped.weight.redistribute()
             return wrapped
 
         for name, layer in module.named_children():
             unfused_layer = apply_unfuse_weights(layer)
             setattr(module, name, unfused_layer)
+        
+        if hasattr(module, "weight") and isinstance(module.weight, torch.Tensor):
+            if module.weight.is_distributed():
+                module.weight = module.weight.redistribute()
         return module
